fe2bb23faead5f9d1b0a048c5ab8d7631c6ac34e
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2026-06-09 16:37:20 -0300

    fix: include about page on router

--- g-pong/src/App.tsx
+++ g-pong/src/App.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { SinglePlayer } from './pages/SinglePlayer';
 import { Multiplayer } from './pages/Multiplayer';
-import './App.css'; // Seus estilos do Tailwind
+import { Sobre } from './pages/Sobre';
+import './App.css';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/singleplayer" element={<SinglePlayer />} />
         <Route path="/multiplayer" element={<Multiplayer />} />
+        <Route path="/sobre" element={<Sobre />} />
       </Routes>
     </Router>
   );
